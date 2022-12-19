4a23136a82da05188f3de2fef90df8976e331c6b
IT-SHERIFE\IT <sherif.hassan@momkn.com.eg>
2022-12-19 19:33:22 +0200

    8th commit 19/12/2022

---
+++ backend/set_env.bat
@@ -0,0 +1,11 @@
+set NODE_ENV=production
+set VERSION=1
+set TYPEORM_CONNECTION=postgres
+set TYPEORM_MIGRATIONS_DIR=./migrations
+set TYPEORM_ENTITIES=.\modules\domain\**\*.entity.ts
+set TYPEORM_MIGRATIONS=.\migrations\*.ts
+set TYPEORM_HOST=localhost
+set TYPEORM_PORT=5532
+set TYPEORM_USERNAME=postgres
+set TYPEORM_PASSWORD=password
+set TYPEORM_DATABASE=glee
